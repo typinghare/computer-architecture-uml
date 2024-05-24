48373aa0215f7deabd162b582cf06da92fc53b53
James Chan <jameschan312.cn@gmail.com>
2024-05-24 16:32:09 -0400

    hw3

--- hw3/rfib.asm
+++ hw3/rfib.asm
@@ -16,7 +16,7 @@ P2:         PUSH            ; Put the return value in the accumulator to the sta
             JUMP LOOP:      ; Go back to the beginning of the loop
 FIB:        LODL 1          ; Load the argument n to the accumulator
             JZER FIBZER:    ; If n == 0 goto FIBZER
-            SUBD 1          ; Decrement the value in the accumulator by 1
+            SUBD c1         ; Decrement the value in the accumulator by 1
             JZER FIBONE:    ; If n == 0 goto FIBONE
             PUSH            ; Push (n - 1) to the stack
             CALL FIB:       ; Call the fib function (recursion) to calculate fib(n - 1)
@@ -26,7 +26,7 @@ FIB:        LODL 1          ; Load the argument n to the accumulator
             PUSH            ; Push (n - 2) to the stack
             CALL FIB:       ; Call the fib function (recurssion) to calculate fib(n - 2)
             ADDL 2          ; ac += fib(n - 1), where ac = fib(n - 2)
-            LODL 3          ; Clear the stack
+            INSP 3          ; Clear the stack
             RETN            ; Return
 FIBZER:     LODD c0:        ; Load 0 to the accumulator; f(0) = 0
 	        RETN			; Return
@@ -34,14 +34,18 @@ FIBONE:	    LODD c1:        ; Load 1 to the accumulator; f(1) = 1
 	        RETN			; Return
 DONE:	    HALT            ; Halt
 .LOC 	    100			    ; Locate data beginning at 100
-c0: 	    0			    ; 0 (constant)
-c1: 	    1               ; 1 (constant)
-d0:  	    3			    ; Array of args for the fib function
-f0:  	    0			    ; Array of result locations for fib results
-     	    0
-     	    0
-     	    0
-     	    0
-daddr:      d0:			    ; Start address of fib args
-faddr:      f0:			    ; Start address of fib results
-count:      1			    ; The number of the remaining data elements to process
+c0: 	    0			    ; [100] 0 (constant)
+c1: 	    1               ; [101] 1 (constant)
+d0:  	    3			    ; [102] Array of args for the fib function
+     	    9               ; [103]
+     	    0               ; [104]
+     	    0               ; [105]
+     	    0               ; [106]
+f0:  	    0			    ; [107] Array of result locations for fib results
+     	    0               ; [108]
+     	    0               ; [109]
+     	    0               ; [110]
+     	    0               ; [111]
+daddr:      d0:			    ; [112] Start address of fib args
+faddr:      f0:			    ; [113] Start address of fib results
+count:      1			    ; [114] The number of the remaining data elements to process

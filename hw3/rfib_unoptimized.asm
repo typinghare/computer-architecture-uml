e5c9a596f455ec9e26f45cb0757032b796a3513a
James Chan <jameschan312.cn@gmail.com>
2024-05-24 18:32:39 -0400

    hw3

--- hw3/rfib_unoptimized.asm
+++ hw3/rfib_unoptimized.asm
@@ -14,14 +14,10 @@ P2:         PUSH            ; Put the return value in the accumulator to the sta
             ADDD c1:        ; [Assignment] ac += 1
             STOD faddr:     ; [Assignment] faddr += 1
             JUMP LOOP:      ; Go back to the beginning of the loop
-FIB:        LODD memoaddr:  ; Load the address of the memo array
-            ADDL 1          ; Caculate the address of memo[n]
-            PSHI            ; Push memo[n] to the stack
-            JNEG FIB_CALC:  ; If memo[n] == -1, the value has yet been calculated
-            POP             ; Pop the value, fib(n), to the accumulator
-            RETN            ; Return
-FIB_CALC:   LODL 1          ; Load the argument n to the accumulator
+FIB:        LODL 1          ; Load the argument n to the accumulator
+            JZER FIBZER:    ; If n == 0 goto FIBZER
             SUBD c1:        ; Decrement the value in the accumulator by 1
+            JZER FIBONE:    ; If (n - 1) == 0 goto FIBONE
             PUSH            ; Push (n - 1) to the stack
             CALL FIB:       ; Call the fib function (recursion) to calculate fib(n - 1)
             PUSH            ; Push the return value, fib(n - 1), to the stack
@@ -31,12 +27,6 @@ FIB_CALC:   LODL 1          ; Load the argument n to the accumulator
             CALL FIB:       ; Call the fib function (recurssion) to calculate fib(n - 2)
             ADDL 1          ; ac += fib(n - 1), where ac = fib(n - 2)
             INSP 3          ; Clear the stack
-            PUSH            ; Push fib(n) to the stack
-            LODD memoaddr:  ; Load the address of the memo array
-            ADDL 2          ; Caculate the address of memo[n]
-            POPI            ; Save fib(n) to &memo[n] => memo[n] = fib(n)
-            PSHI            ; Push memo[n] to the stack again
-            POP             ; Pop memo[n] and save the value to the accumulator
             RETN            ; Return
 FIBZER:     LODD c0:        ; Load 0 to the accumulator => fib(0) = 0
 	        RETN			; Return
@@ -59,30 +49,3 @@ f0:  	    0			    ; [107] Array of result locations for fib results
 daddr:      d0:			    ; [112] Start address of fib args
 faddr:      f0:			    ; [113] Start address of fib results
 count:      1			    ; [114] The number of the remaining data elements to process
-memo:       0               ; [115] memo[0] The memo array to store fib(n), where 0 <= n <= 25
-            1               ; [116] memo[1]
-            -1              ; [117] memo[2]
-            -1              ; [118] memo[3]
-            -1              ; [119] memo[4]
-            -1              ; [120] memo[5]
-            -1              ; [121] memo[6]
-            -1              ; [122] memo[7]
-            -1              ; [123] memo[8]
-            -1              ; [124] memo[9]
-            -1              ; [125] memo[10]
-            -1              ; [126] memo[11]
-            -1              ; [127] memo[12]
-            -1              ; [128] memo[13]
-            -1              ; [129] memo[14]
-            -1              ; [130] memo[15]
-            -1              ; [131] memo[16]
-            -1              ; [132] memo[17]
-            -1              ; [133] memo[18]
-            -1              ; [134] memo[19]
-            -1              ; [135] memo[20]
-            -1              ; [136] memo[21]
-            -1              ; [137] memo[22]
-            -1              ; [138] memo[23]
-            -1              ; [139] memo[24]
-            -1              ; [140] memo[25]
-memoaddr:   memo:           ; The address of the memo array

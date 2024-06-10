f2afe4922fc9c202108ac8f6953fc3a7eaa57715
James Chan <jameschan312.cn@gmail.com>
2024-06-09 20:09:55 -0400

    hw5

--- hw5/addition.asm
+++ hw5/addition.asm
@@ -1,13 +1,20 @@
-Start:          LODD on:            ;
+Start:
+
+PrintPrompt:    LODD on:            ;
                 STOD 4095           ;
                 CALL BusyWrite:     ;
                 LOCO prompt:        ;
+                CALL PrintStr       ; Print the prompt string
+                CALL ScanNum        ; Scan the first number
+                CALL PrintStr       ; Print the prompt string
+                CALL ScanNum        ; Scan the second number
+                CALL AddNums        ; Add the two numbers
 
 ; @brief Prints a string.
 ; @param r[ac] The address of the string to print.
 PrintStr:       PSHI                ; Push the first 2-chars to stack
                 ADDD c1:            ; Increment the address in AC by 1
-                STOD strptr:        ; Save the address of the next 2-chars
+                STOD str_ptr:       ; Save the address of the next 2-chars
                 POP                 ; Pop the first 2-chars to AC
                 JZER PrintCRLF:     ; End printing if the character is '\0'
                 STOD 4094           ; Store the lower character to the buffer
@@ -21,10 +28,10 @@ PrintStr:       PSHI                ; Push the first 2-chars to stack
                 POP                 ; Pop the swapeed 2-chars to AC
                 STOD 4094           ; Store the higher character to the buffer
                 CALL BusyWrite:     ; Wait until it is printed
-                LODD strptr:        ; Load the address of the next 2-chars to AC
+                LODD str_ptr:       ; Load the address of the next 2-chars to AC
                 JUMP PrintStr:      ; Continue to print the string
 CPrintCRLF:     INSP 1              ; Clean up the stack before printing CRLF
-PrintCRLF:      LODD asciicr:       ; The following prints "\r\n".
+PrintCRLF:      LODD ascii_cr:      ; The following prints "\r\n".
                 STOD 4094           ;
                 CALL BusyWrite:     ; Print '\r'
                 LODD asciinl:       ;
@@ -32,41 +39,40 @@ PrintCRLF:      LODD asciicr:       ; The following prints "\r\n".
                 CALL BusyWrite:     ; Print '\n'
                 LODD on:            ;
                 STOD 4093           ; Let MIC-1 program print the string
+                RETN                ; Return
 
 ; @brief Scans in a 1-5 digit number. The number will be stored to either num1
-;        or num2 according to numPtr.
+;        or num2 according to num_ptr.
 ScanNum:        call BusyRead:      ; Read a character
                 LODD 4092           ; Lodd the character to AC
                 SUBD ascii0:        ; Convert it into the corresponding digit
                 PUSH                ; Push the digit to the stack
 NextDigit:      CALL BusyRead:      ; Read a character
                 LODD 4092           ;
-                STOD nextChar:      ; Store the character
+                STOD next_char:     ; Store the character
                 SUBD asciinl:       ;
                 JZER EndNum:        ; If the character is '\n', it ends reading
                 MULT 10             ; Base 10 left shift
-                LODD nextChar:      ; Load the next character to AC
+                LODD next_char:     ; Load the next character to AC
                 SUBD ascii0:        ; Convert it into the corresponding digit
                 ADDL 0              ; Add m[sp] to it
                 STOL 0              ; Store the result to m[sp]
                 JUMP NextDigit:     ; Continue to read the next digit
 
-; @brief Increments numPtr and decrements numCount; Jumps to AddNums if
+; @brief Increments num_ptr and decrements num_count; Jumps to AddNums if
 ;        numnCount == 0, otherwise reads the other number.
-EndNum:         LODD numPtr:        ;
-                POPI                ; Pop and store the value to numPtr
+EndNum:         LODD num_ptr:       ;
+                POPI                ; Pop and store the value to num_ptr
                 ADDD c1:            ;
-                STOD numPtr:        ; numPtr++;
-                LODD numCount:      ;
-                JZER AddNum:        ; Add the two numbers if numCount == 0
+                STOD num_ptr:       ; num_ptr++;
+                LODD num_count:     ;
+                JZER AddNums:       ; Add the two numbers if num_count == 0
                 SUBD c1:            ;
-                STOD numCount:      ; numCount--;
-                JUMP Start:         ; Read the other number
-; @brief Adds the two numbers.
-AddNum:         HALT                ;
+                STOD num_count:     ; num_count--;
+                RETN                ; Return
 
 ; @brief Writes a character to the buffer; wait until m[4095] >= 10.
-BusyWrite:      LODD 4095
+BusyWrite:      LODD 4095           ;
                 SUBD asciinl:       ;
                 JNEG BusyWrite:     ;
                 RETN                ; Return
@@ -82,7 +88,7 @@ BusyRead:       LODD 4093           ; Buzy waiting read
 SwapChars:      LOCO 8              ; ac := 8
 Loop1:          JZER Finish:        ;
                 SUBD c1:            ;
-                STOD loopCount:     ; loopCount--;
+                STOD loop_count:    ; loop_count--;
                 LODL 1              ; ac = m[sp + 1]
                 JNEG Add1:          ; If MSB == 1, add 1 after the shift
                 ADDL 1              ; Equivalent to: ac <<= 1
@@ -90,26 +96,29 @@ Loop1:          JZER Finish:        ;
 Add1:           ADDL 1              ; Equivalent to: ac <<= 1
                 ADDD c1:            ; ac++;
 StoreNewChars:  STOL 1              ; m[sp + 1] = ac
-                LODD loopCount:     ; Continue next loop
+                LODD loop_count:    ; Continue next loop
                 JUMP Loop1:         ;
 Finish:         LODL 1              ; ac := m[sp + 1]
                 RETN                ; Return m[sp + 1]
 
+; @brief Adds the two numbers. The result is stored to AC.
+AddNums:        HALT                ;
+
 .LOC 200                            ; <Constants>
 on:             8                   ; MIC-1 on signal
-ascii0:         48                  ; '0'
-asciinl:        10                  ; '\n'
-asciicr:        13                  ; '\r'
+ascii_0:        48                  ; '0'
+ascii_nl:       10                  ; '\n'
+ascii_cr:       13                  ; '\r'
 c1:             1                   ; Constant 1
 c10:            10                  ; Constant 10
 c255:           255                 ; Constant 255
 prompt:         "Please input a 1-5 digit number followed by enter: "
 
 .LOC 500                            ; <Variables>
-strptr:         0                   ; Pointer to character to print
-nextChar:       0                   ; Next character
+str_ptr:        0                   ; Pointer to character to print
+next_char:      0                   ; Next character
 num1:           0                   ; The first addend
 num2:           0                   ; The second addend
-numPtr:         num1:               ; Pointer to the number to process
-numCount:       1                   ; The count of remaining numbers to read
-loopCount:      0                   ; Loop counts used in SwapChars
+num_ptr:        num1:               ; Pointer to the number to process
+num_count:      1                   ; The count of remaining numbers to read
+loop_count:     0                   ; Loop counts used in SwapChars

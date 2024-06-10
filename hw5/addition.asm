a11d7a01581460058fd0a178eb247d16138aabd3
James Chan <jameschan312.cn@gmail.com>
2024-06-10 00:12:21 -0400

    hw5

--- hw5/addition.asm
+++ hw5/addition.asm
@@ -35,7 +35,7 @@ CPrintCRLF:     INSP 1              ; Clean up the stack before printing CRLF
 PrintCRLF:      LODD ascii_cr:      ; The following prints "\r\n".
                 STOD 4094           ;
                 CALL BusyWrite:     ; Print '\r'
-                LODD ascii_nl:       ;
+                LODD ascii_nl:      ;
                 STOD 4094           ;
                 CALL BusyWrite:     ; Print '\n'
                 LODD on:            ;
@@ -46,16 +46,16 @@ PrintCRLF:      LODD ascii_cr:      ; The following prints "\r\n".
 ;        or num2 according to num_ptr.
 ScanNum:        call BusyRead:      ; Read a character
                 LODD 4092           ; Lodd the character to AC
-                SUBD ascii0:        ; Convert it into the corresponding digit
+                SUBD ascii_0:       ; Convert it into the corresponding digit
                 PUSH                ; Push the digit to the stack
 NextDigit:      CALL BusyRead:      ; Read a character
                 LODD 4092           ;
                 STOD next_char:     ; Store the character
-                SUBD ascii_nl:       ;
+                SUBD ascii_nl:      ;
                 JZER EndNum:        ; If the character is '\n', it ends reading
                 MULT 10             ; Base 10 left shift
                 LODD next_char:     ; Load the next character to AC
-                SUBD ascii0:        ; Convert it into the corresponding digit
+                SUBD ascii_0:       ; Convert it into the corresponding digit
                 ADDL 0              ; Add m[sp] to it
                 STOL 0              ; Store the result to m[sp]
                 JUMP NextDigit:     ; Continue to read the next digit
@@ -74,13 +74,13 @@ EndNum:         LODD num_ptr:       ;
 
 ; @brief Writes a character to the buffer; wait until m[4095] >= 10.
 BusyWrite:      LODD 4095           ;
-                SUBD ascii_nl:       ;
+                SUBD ascii_nl:      ;
                 JNEG BusyWrite:     ;
                 RETN                ; Return
 
 ; @brief Reads a character into the buffer; wait until m[4093] >= 10.
-BusyRead:       LODD 4093           ; Buzy waiting read
-                SUBD ascii_nl:       ;
+BusyRead:       LODD 4093           ;
+                SUBD ascii_nl:      ;
                 JNEG BusyRead:      ;
                 RETN                ; Return
 
